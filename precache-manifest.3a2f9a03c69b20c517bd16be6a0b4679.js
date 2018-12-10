85353863849701491161bbf24dfbd3fb96374833
mlizchap <mlizchap@gmail.com>
2018-12-10 09:58:32 -0500

    Updates

---
+++ precache-manifest.3a2f9a03c69b20c517bd16be6a0b4679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be93f7a4a133a644365e396d503cd769",
+    "url": "/photo-filters/static/media/image1.be93f7a4.jpg"
+  },
+  {
+    "revision": "e3f32397343657e99885",
+    "url": "/photo-filters/static/js/runtime~main.e3f32397.js"
+  },
+  {
+    "revision": "2c940e33a609fb6d6634",
+    "url": "/photo-filters/static/js/main.2c940e33.chunk.js"
+  },
+  {
+    "revision": "cf251b0f551c3eae5202",
+    "url": "/photo-filters/static/js/1.cf251b0f.chunk.js"
+  },
+  {
+    "revision": "2c940e33a609fb6d6634",
+    "url": "/photo-filters/static/css/main.7af5fd4b.chunk.css"
+  },
+  {
+    "revision": "8aa1512f6320604a5dbb2a9d3b714e18",
+    "url": "/photo-filters/index.html"
+  }
+];
