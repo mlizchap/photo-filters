d3da02b353dc70e9edf869e9b5dec6c217b0e61a
mlizchap <40478204+mlizchap@users.noreply.github.com>
2018-12-14 22:31:56 -0500

    test
    test

--- src/style.js
+++ src/style.js
@@ -1,47 +1,46 @@
 export const theme = {
-    subsectionBgPadding: '10px',
-    borderRadius: '3px',
+  subsectionBgPadding: "10px",
+  borderRadius: "3px",
 
-    // fonts
-    titleFont: 'Poppins',
-    mainFont: 'Quicksand',
+  // fonts
+  titleFont: "Arial",
+  mainFont: "Quicksand",
 
-    // colors
-    lightBg: '#424242',
-    darkBg: '#1A222A',
-    containerBg: '#394551',
-    // lightestBg: '#3b4149',
-    dark: '#474747',
-    presets: {
-        main: '#6CFFC2', // green
-        hovered: '#94fcd1',
-        light: '#d8ffef',
-        highlighted: '#5bcc9d',
-        dark: '#0e5b3b'
-    },
-    tints: {
-        main: '#75C5FF', // blue
-        hovered: '#add6f4',
-        light: '#c4e5fc',
-        dark: '#0c3f63',
-        hovered: '#c6e7ff',
-        highlighted: '#5495c4',
-    },
-    code: {
-        main: '#f9b1d8', //pink
-        dark: '#6d3050',
-        light: '#ffe0f0',
-    },
-    filters: {
-        main: '#AD93F0', // purple
-        dark: '#3E326E',
-        highlighted: '#73639b',
-        hovered: '#c1aeef',
-        light: '#eeeaf7',
-        code: '#e1d6ff'
-    },
-    highlight: '#f7edc3',
-    highlight_dull: '#f9f4e0',
-    highlightDark: '#e0a06b',
-
-}
+  // colors
+  lightBg: "#424242",
+  darkBg: "#1A222A",
+  containerBg: "#394551",
+  // lightestBg: '#3b4149',
+  dark: "#474747",
+  presets: {
+    main: "#6CFFC2", // green
+    hovered: "#94fcd1",
+    light: "#d8ffef",
+    highlighted: "#5bcc9d",
+    dark: "#0e5b3b"
+  },
+  tints: {
+    main: "#75C5FF", // blue
+    hovered: "#add6f4",
+    light: "#c4e5fc",
+    dark: "#0c3f63",
+    hovered: "#c6e7ff",
+    highlighted: "#5495c4"
+  },
+  code: {
+    main: "#f9b1d8", //pink
+    dark: "#6d3050",
+    light: "#ffe0f0"
+  },
+  filters: {
+    main: "#AD93F0", // purple
+    dark: "#3E326E",
+    highlighted: "#73639b",
+    hovered: "#c1aeef",
+    light: "#eeeaf7",
+    code: "#e1d6ff"
+  },
+  highlight: "#f7edc3",
+  highlight_dull: "#f9f4e0",
+  highlightDark: "#e0a06b"
+};
